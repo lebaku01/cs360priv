4c1f363a47c7a2e4cf3fc3422679905be07df919
Kurt Lebakken <kurt@lebakken.com>
2024-09-26 20:22:02 -0500

    adding hashing.py again

--- src/exercises/hashing/hashing.py
+++ src/exercises/hashing/hashing.py
@@ -22,7 +22,6 @@ def hash_remainder(key: int, size: int) -> int:
     return key % size
 
 
-
 def hash_mid_sqr(key: int, size: int) -> int:
     """
     Finds hash using mid-square method
@@ -36,8 +35,12 @@ def hash_mid_sqr(key: int, size: int) -> int:
     >>> hash_mid_sqr(424, 9)
     7
     """
-    # TODO: Implement this function
-    ...
+    square = pow(key, 2)
+    key = str(square)
+    if len(key) % 2 != 0:
+        key = str(0) + key
+    mid = len(key) // 2
+    return int(key[mid-1:mid+1]) % size
 
 
 def hash_folding(key: str, size: int) -> int:
@@ -53,8 +56,25 @@ def hash_folding(key: str, size: int) -> int:
     >>> hash_folding(424-7-23, 8)
     3
     """
-    # TODO: Implement this function
-    ...
+    key = str(key)  # cast to string so I can index the input
+    index = 0
+    pairs = []
+    numbers = 0
+    while index < len(str(key)):  # parse the string, build a list of strings of only pairs of digits
+        if key[index].isnumeric() and (numbers + 1) % 2 != 0:
+            pairs.append([key[index]])
+            numbers += 1
+        elif key[index].isnumeric():
+            pairs[len(pairs) - 1][0] += key[index]
+            numbers += 1
+        else:  # key[index] is non-numeric
+            pass  # do nothing
+        index += 1
+
+    string_sum = 0  # the list now looks something like ['12','34','5'], now we sum up the strings
+    for pair in pairs:
+        string_sum += int(pair[0])
+    return string_sum % size  # take mod and return
 
 
 def hash_str(key: str, size: int) -> int:
@@ -70,8 +90,10 @@ def hash_str(key: str, size: int) -> int:
     >>> hash_str('vardakar', 7)
     4
     """
-    # TODO: Implement this function
-    ...
+    string_sum = 0
+    for string in key:
+        string_sum += ord(string)
+    return string_sum % size
 
 
 def hash_str_weighted(key: str, size: int) -> int:
@@ -87,5 +109,10 @@ def hash_str_weighted(key: str, size: int) -> int:
     >>> hash_str_weighted('vardakar', 7)
     2
     """
-    # TODO: Implement this function
-    ...
+    string_sum = 0
+    index = 0
+    for string in key:
+        string_sum += ord(string) * index
+        index += 1
+    return string_sum % size
+

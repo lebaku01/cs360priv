912daf249ac3c5b964ddd1aa272ce8ef68aa7626
Kurt Lebakken <kurt@lebakken.com>
2024-10-06 14:06:24 -0500

    add trees traversal solution

--- src/exercises/trees/traversal.py
+++ src/exercises/trees/traversal.py
@@ -19,8 +19,24 @@ def get_preorder(inorder: str, postorder: str) -> str:
     >>> get_preorder("UOMELBARTKGSNI", "UMELABORSGNIKT")
     'TROUBLEMAKINGS'
     """
-    # TODO: Implement this function
-    ...
+    #grab root, left and right subtrees in both inorder and postorder
+    root = postorder[-1]
+    inorder_subtrees = inorder.split(postorder[len(postorder) - 1])
+    postorder = postorder[:len(postorder) - 1]
+    postorder_subtrees = [postorder[0:len(inorder_subtrees[0])],
+                          postorder[len(inorder_subtrees[0]):]]
+    #base case : two empty subtrees
+    if inorder_subtrees[0] == inorder_subtrees[1] == "":
+        return root
+    #recursive cases, 1) right subtree empty 2) left subtree empty 3) both subtrees non-empty
+    elif inorder_subtrees[1] == "":
+        return root + get_preorder(inorder_subtrees[0], postorder_subtrees[0])
+    elif inorder_subtrees[0] == "":
+        return root + get_preorder(inorder_subtrees[1], postorder_subtrees[1])
+    else:
+        return (root + get_preorder(inorder_subtrees[0], postorder_subtrees[0])
+                + get_preorder(inorder_subtrees[1], postorder_subtrees[1]))
+
 
 
 def main():

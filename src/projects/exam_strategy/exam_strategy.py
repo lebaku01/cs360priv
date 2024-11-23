91821b752c0ecd8ea94dd24be624727382ed6e7e
Kurt Lebakken <kurt@lebakken.com>
2024-11-23 14:17:45 -0600

    added file parsing to the exam_strategy.py file

--- src/projects/exam_strategy/exam_strategy.py
+++ src/projects/exam_strategy/exam_strategy.py
@@ -18,6 +18,9 @@ def knapsack(capacity: int, items: list[Item]) -> list[int]:
     :param capacity: total knapsack capacity
     :param items: the list of items (named tuples) to consider
     :return: a list of chosen indices
+
+    knapsack algorithm pseudocode:
+
     """
     # TODO: Implement this function
     ...
@@ -25,18 +28,39 @@ def knapsack(capacity: int, items: list[Item]) -> list[int]:
 
 def pick_questions_to_answer(filename: str) -> tuple[list[int], int]:
     """
-    Main selection function
-
+    Main selection function:
+    - info on how the files are formatted
+        Each input file describes questions of such exam. The first line of the file contains two numbers,
+        a floating point t (exam time limit), and an integer n (number of exam questions). The rest of the
+        file contains the value (points) and weight (time complexity) of each of the question (item).
+        Both values and weights will be integers.
     :param filename: file to process
     :return: the list of chosen indices and total point value of all selected questions
     """
-    # TODO: Implement this function
-    ...
+    questions, exam_information = parse_file(filename)
+    # implement knapsack from here
+
+
+def parse_file(filename: str) -> tuple[list[tuple[int, int]], dict]:
+    file = open(filename, "r")
+    lines = file.readlines()
+    # first line of the file
+    exam_information = dict()
+    exam_information["time_limit"], exam_information["number_of_questions"] = lines[0].split(" ")
+
+    # rest of file
+    questions = []
+    for line in lines[1:]:
+        points, weight = line.split(" ")
+        questions.append((points, weight))
+
+    return questions, exam_information
 
 
 def main():
     """Entry point"""
     for i in range(1, 6):
+        ###for debugging
         filename = f"data/projects/exam_strategy/questions{i}.in"
         selection = pick_questions_to_answer(filename)
         print(
